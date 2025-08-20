703ed6c0f5eca220de8b088d67bde17d6f0d4c9c
yashendy <yashendy@gmail.com>
2025-08-20 10:23:06 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,25 +1,17 @@
 // firebase-config.js
-
-// 🧠 استيراد الوظائف الأساسية من Firebase (بدون storage)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// 🔥 إعدادات Firebase الخاصة بمشروعك
 const firebaseConfig = {
-  apiKey: "AIzaSyBs6rFN0JH26Yz9tiGdBcFK8ULZ2zeXiq4",
+  apiKey: "YOUR_API_KEY",
   authDomain: "sugar-kids-tracker.firebaseapp.com",
   projectId: "sugar-kids-tracker",
-  storageBucket: "sugar-kids-tracker.appspot.com", // ده مش هنستخدمه لكن يفضل تسيبيه لو هتضيفيه بعدين
-  messagingSenderId: "251830888114",
-  appId: "1:251830888114:web:a20716d3d4ad86a6724bab",
-  measurementId: "G-L7YGX3PHLB"
+  appId: "YOUR_APP_ID"
 };
 
-// ✅ تهيئة التطبيق والخدمات
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// 📤 تصدير المتغيرات لاستخدامها في ملفات تانية
 export { auth, db };
