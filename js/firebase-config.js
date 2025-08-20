4d811f2444bd1ae2d5512ecfb2751ab2175d5579
yashendy <yashendy@gmail.com>
2025-08-20 10:37:29 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,10 +4,10 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBs6rFN0JH26Yz9tiGdBcFK8ULZ2zeXiq4",
   authDomain: "sugar-kids-tracker.firebaseapp.com",
   projectId: "sugar-kids-tracker",
-  appId: "YOUR_APP_ID"
+  appId: "1:251830888114:web:a20716d3d4ad86a6724bab"
 };
 
 const app = initializeApp(firebaseConfig);
